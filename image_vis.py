555d7cb5bc726f215db3e26f87eb451b565c61a4
chandlevier <116505208+chandlevier@users.noreply.github.com>
2023-03-23 17:52:30 +0800

    Add files via upload

---
+++ image_vis.py
@@ -0,0 +1,303 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import copy
+
+import cv2
+import numpy as np
+import torch
+from matplotlib import pyplot as plt
+import math
+import os
+
+
+def project_pts_on_img(points,
+                       raw_img,
+                       lidar2img_rt,
+                       max_distance=70,
+                       thickness=-1):
+    """Project the 3D points cloud on 2D image.
+
+    Args:
+        points (numpy.array): 3D points cloud (x, y, z) to visualize.
+        raw_img (numpy.array): The numpy array of image.
+        lidar2img_rt (numpy.array, shape=[4, 4]): The projection matrix
+            according to the camera intrinsic parameters.
+        max_distance (float, optional): the max distance of the points cloud.
+            Default: 70.
+        thickness (int, optional): The thickness of 2D points. Default: -1.
+    """
+    img = raw_img.copy()
+    num_points = points.shape[0]
+    pts_4d = np.concatenate([points[:, :3], np.ones((num_points, 1))], axis=-1)
+    pts_2d = pts_4d @ lidar2img_rt.T
+
+    # cam_points is Tensor of Nx4 whose last column is 1
+    # transform camera coordinate to image coordinate
+    pts_2d[:, 2] = np.clip(pts_2d[:, 2], a_min=1e-5, a_max=99999)
+    pts_2d[:, 0] /= pts_2d[:, 2]
+    pts_2d[:, 1] /= pts_2d[:, 2]
+
+    fov_inds = ((pts_2d[:, 0] < img.shape[1])
+                & (pts_2d[:, 0] >= 0)
+                & (pts_2d[:, 1] < img.shape[0])
+                & (pts_2d[:, 1] >= 0))
+
+    imgfov_pts_2d = pts_2d[fov_inds, :3]  # u, v, d
+
+    cmap = plt.cm.get_cmap('hsv', 256)
+    cmap = np.array([cmap(i) for i in range(256)])[:, :3] * 255
+    for i in range(imgfov_pts_2d.shape[0]):
+        depth = imgfov_pts_2d[i, 2]
+        color = cmap[np.clip(int(max_distance * 10 / depth), 0, 255), :]
+        cv2.circle(
+            img,
+            center=(int(np.round(imgfov_pts_2d[i, 0])),
+                    int(np.round(imgfov_pts_2d[i, 1]))),
+            radius=1,
+            color=tuple(color),
+            thickness=thickness,
+        )
+    cv2.imshow('project_pts_img', img.astype(np.uint8))
+    cv2.waitKey(100)
+
+
+def plot_rect3d_on_img(img,
+                       num_rects,
+                       rect_corners,        # 每个bbox的8个角点的三维空间坐标数组转换的二维坐标数组
+                       color=(0, 255, 0),   # bbox颜色固定为蓝色
+                       thickness=1):
+    """Plot the boundary lines of 3D rectangular on 2D images.
+
+    Args:
+        img (numpy.array): The numpy array of image.
+        num_rects (int): Number of 3D rectangulars.
+        rect_corners (numpy.array): Coordinates of the corners of 3D
+            rectangulars. Should be in the shape of [num_rect, 8, 2].
+        color (tuple[int], optional): The color to draw bboxes.
+            Default: (0, 255, 0).
+        thickness (int, optional): The thickness of bboxes. Default: 1.
+    """
+    line_indices = ((0, 1), (0, 3), (0, 4), (1, 2), (1, 5), (3, 2), (3, 7),
+                    (4, 5), (4, 7), (2, 6), (5, 6), (6, 7))
+    for i in range(num_rects):
+        corners = rect_corners[i].astype(np.int)
+        orgx = rect_corners[i][0][0]
+        orgy = rect_corners[i][0][1]
+        font = cv2.FONT_HERSHEY_COMPLEX
+        cv2.putText(img, str(i+1), (int(orgx), int(orgy)), font, 0.8, (255,0,0), 1, cv2.LINE_AA)
+        for start, end in line_indices:
+            cv2.line(img, (corners[start, 0], corners[start, 1]),
+                     (corners[end, 0], corners[end, 1]), color, thickness,
+                     cv2.LINE_AA)
+
+    return img.astype(np.uint8)
+
+
+def draw_lidar_bbox3d_on_img(bboxes3d,
+                             raw_img,
+                             lidar2img_rt,
+                             img_metas,
+                             color=(0, 255, 0),
+                             thickness=1):
+    """Project the 3D bbox on 2D plane and draw on input image.
+
+    Args:
+        bboxes3d (:obj:`LiDARInstance3DBoxes`):
+            3d bbox in lidar coordinate system to visualize.
+        raw_img (numpy.array): The numpy array of image.
+        lidar2img_rt (numpy.array, shape=[4, 4]): The projection matrix
+            according to the camera intrinsic parameters.
+        img_metas (dict): Useless here.
+        color (tuple[int], optional): The color to draw bboxes.
+            Default: (0, 255, 0).
+        thickness (int, optional): The thickness of bboxes. Default: 1.
+    """
+    img = raw_img.copy()
+    corners_3d = bboxes3d.corners
+    num_bbox = corners_3d.shape[0]
+    pts_4d = np.concatenate(
+        [corners_3d.reshape(-1, 3),
+         np.ones((num_bbox * 8, 1))], axis=-1)
+    lidar2img_rt = copy.deepcopy(lidar2img_rt).reshape(4, 4)
+    if isinstance(lidar2img_rt, torch.Tensor):
+        lidar2img_rt = lidar2img_rt.cpu().numpy()
+    pts_2d = pts_4d @ lidar2img_rt.T
+
+    pts_2d[:, 2] = np.clip(pts_2d[:, 2], a_min=1e-5, a_max=1e5)
+    pts_2d[:, 0] /= pts_2d[:, 2]
+    pts_2d[:, 1] /= pts_2d[:, 2]
+    imgfov_pts_2d = pts_2d[..., :2].reshape(num_bbox, 8, 2)
+
+    return plot_rect3d_on_img(img, num_bbox, imgfov_pts_2d, color, thickness)
+
+
+# TODO: remove third parameter in all functions here in favour of img_metas
+def draw_depth_bbox3d_on_img(bboxes3d,
+                             raw_img,
+                             calibs,
+                             img_metas,
+                             color=(0, 255, 0),
+                             thickness=1):
+    """Project the 3D bbox on 2D plane and draw on input image.
+
+    Args:
+        bboxes3d (:obj:`DepthInstance3DBoxes`, shape=[M, 7]):
+            3d bbox in depth coordinate system to visualize.
+        raw_img (numpy.array): The numpy array of image.
+        calibs (dict): Camera calibration information, Rt and K.
+        img_metas (dict): Used in coordinates transformation.
+        color (tuple[int], optional): The color to draw bboxes.
+            Default: (0, 255, 0).
+        thickness (int, optional): The thickness of bboxes. Default: 1.
+    """
+    from mmdet3d.core.bbox import points_cam2img
+    from mmdet3d.models import apply_3d_transformation
+
+    img = raw_img.copy()
+    img_metas = copy.deepcopy(img_metas)
+    corners_3d = bboxes3d.corners
+    num_bbox = corners_3d.shape[0]
+    points_3d = corners_3d.reshape(-1, 3)
+
+    # first reverse the data transformations
+    xyz_depth = apply_3d_transformation(
+        points_3d, 'DEPTH', img_metas, reverse=True)
+
+    # project to 2d to get image coords (uv)
+    uv_origin = points_cam2img(xyz_depth,
+                               xyz_depth.new_tensor(img_metas['depth2img']))
+    uv_origin = (uv_origin - 1).round()
+    imgfov_pts_2d = uv_origin[..., :2].reshape(num_bbox, 8, 2).numpy()
+
+    return plot_rect3d_on_img(img, num_bbox, imgfov_pts_2d, color, thickness)
+
+
+def draw_camera_bbox3d_on_img(bboxes3d,
+                              raw_img,
+                              cam2img,
+                              bev_file, 
+                              frame, cam,
+                              img_metas=None,
+                              color=(0, 255, 0),
+                              thickness=1,
+                              ):
+    """Project the 3D bbox on 2D plane and draw on input image.
+
+    Args:
+        bboxes3d (:obj:`CameraInstance3DBoxes`, shape=[M, 7]):
+            3d bbox in camera coordinate system to visualize.
+        raw_img (numpy.array): The numpy array of image.
+        cam2img (dict): Camera intrinsic matrix,
+            denoted as `K` in depth bbox coordinate system.
+        img_metas (dict): Useless here.
+        color (tuple[int], optional): The color to draw bboxes.
+            Default: (0, 255, 0).
+        thickness (int, optional): The thickness of bboxes. Default: 1.
+    """
+    # points_cam2img 将相机坐标系下的点云坐标转换到图像坐标系下
+    # 输入：相机坐标系下的点云、相机内参矩阵、是否包含深度信息
+    # 输出：图像坐标系下的点云
+    from mmdet3d.core.bbox import points_cam2img
+
+    img = raw_img.copy()
+    cam2img = copy.deepcopy(cam2img)
+    corners_3d = bboxes3d.corners
+    # print(corners_3d)
+    # 每个3Dbbox的八个角点，是三维数组，第一维是数量，第二维是不同bbox的角点，第三维是角点坐标
+    num_bbox = corners_3d.shape[0]
+    # print(num_bbox)
+    # 图像上3Dbbox的数量
+    points_3d = corners_3d.reshape(-1, 3)   # 将三维数组整合成二维数组，第一维是所有角点，第二维是角点坐标
+    # print(points_3d)
+    if not isinstance(cam2img, torch.Tensor):
+        cam2img = torch.from_numpy(np.array(cam2img))
+
+    assert (cam2img.shape == torch.Size([3, 3])
+            or cam2img.shape == torch.Size([4, 4]))
+    cam2img = cam2img.float().cpu()
+
+    # project to 2d to get image coords (uv)
+    # 将所有角点的三维空间坐标转化为图像平面上的二维坐标
+    uv_origin = points_cam2img(points_3d, cam2img)
+    # 将二维坐标减一再四舍五入取整
+    uv_origin = (uv_origin - 1).round()
+    # 之前将所有角点整合到一个数组中，现在将不同bbox的角点分在一个数组的不同元素中
+    imgfov_pts_2d = uv_origin[..., :2].reshape(num_bbox, 8, 2).numpy()
+    # 输出二维坐标系下的各个bbox的长宽高
+    # for i in range(num_bbox):
+    #     height = imgfov_pts_2d[i][3][1] - imgfov_pts_2d[i][0][1]
+    #     length = imgfov_pts_2d[i][1][0] - imgfov_pts_2d[i][0][0]
+    #     width = imgfov_pts_2d[i][4][0] - imgfov_pts_2d[i][0][0]
+    #     print("For the %d 3D bounding box, the height is %d, the length is %d, the width is %d"
+    #              % ((i+1), height, length, width))
+    # 输出三维坐标系下的各个bbox的中心坐标和长宽高
+    # center_xoz, center_yoz, center_xoy = tensor(0.0000, 0.0000, 0.0000)
+    image = np.zeros((512, 512, 3), np.uint8)
+    image.fill(255)
+    font = cv2.FONT_HERSHEY_COMPLEX
+    for i in range(num_bbox):
+        x, y, z, l, w, h ,yaw = get_scale_and_center(corners_3d[i])
+        create_bev(image, x, z, l, w, yaw, i, font)
+        # print("The %d 3D bounding box info:\ncenter coordinate:(%.4f, %.4f, %.4f)\nlength = %.4f, width = %.4f, height = %.4f\nangle = %f°\n" % ((i+1), x, y, z, l, w, h, yaw))
+    if not os.path.isdir(os.path.join(bev_file, cam)):
+            os.makedirs(os.path.join(bev_file, cam))
+    bev_filename = bev_file + cam + '/' + frame + "_bev.png"
+    cv2.imwrite(bev_filename, image)
+    
+    return plot_rect3d_on_img(img, num_bbox, imgfov_pts_2d, color, thickness)
+
+
+def get_scale_and_center(corner_3d):
+    h = corner_3d[3][1] - corner_3d[0][1]
+    l = pow(pow((corner_3d[1][0] - corner_3d[0][0]), 2) + pow((corner_3d[1][2] - corner_3d[0][2]), 2), 0.5)
+    w = pow(pow((corner_3d[4][0] - corner_3d[0][0]), 2) + pow((corner_3d[4][2] - corner_3d[0][2]), 2), 0.5)
+    # math.asin返回弧度,这里换算为角度
+    yaw = (math.asin((corner_3d[4][2]-corner_3d[0][2])/w) / math.pi) * 180
+    if yaw < 0:
+        yaw = yaw + 180
+    # 计算中心坐标的代码好像会修改到corner_3d数组,导致之后的长宽高计算不准确,所以需要先计算长宽高再计算坐标
+    x = corner_3d[0][0]
+    y = corner_3d[0][1]
+    z = corner_3d[0][2]
+    for i in range(7):
+        x += corner_3d[i+1][0]
+        y += corner_3d[i+1][1]
+        z += corner_3d[i+1][2]
+    x = x / 8
+    y = y / 8
+    z = z / 8
+
+    return x, y, z, l, w, h, yaw
+
+def create_bev(image, x0, z0, l, w, yaw, i, font):
+    x0 *= 10
+    z0 *= 10
+    l *= 10
+    w *= 10
+    yaw = yaw/180*math.pi
+    x00 = x0 + w*math.cos(yaw)/2
+    z00 = z0 + w*math.sin(yaw)/2
+    x3 = x00 - l*math.sin(yaw)/2
+    z3= z00 + l*math.cos(yaw)/2
+    x2 = x3 + l*math.sin(yaw)
+    z2 = z3 - l*math.cos(yaw)
+    x1 = x2 - w*math.cos(yaw)
+    z1 = z2 - w*math.sin(yaw)
+    x4 = x1 - l*math.sin(yaw)
+    z4 = z1 + l*math.cos(yaw)
+
+    x1, x2, x3, x4 = int(x1), int(x2), int(x3), int(x4)
+    z1, z2, z3, z4 = int(z1), int(z2), int(z3), int(z4)
+
+    x1 += 256
+    x2 += 256
+    x3 += 256
+    x4 += 256
+    z1 = 512 - z1
+    z2 = 512 - z2
+    z3 = 512 - z3
+    z4 = 512 - z4
+    image = cv2.line(image, (x1,z1), (x2,z2), (255,0,0), 1)
+    image = cv2.line(image, (x1,z1), (x4,z4), (255,0,0), 1)
+    image = cv2.line(image, (x3,z3), (x2,z2), (255,0,0), 1)
+    image = cv2.line(image, (x3,z3), (x4,z4), (255,0,0), 1)
+    cv2.putText(image, str(i+1), (int(x1), int(z1)), font, 0.5, (255,0,0), 1, cv2.LINE_AA)
